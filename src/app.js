3f8536c74a77a045a6d9bd81b3068aca259d62df
ulmerkatrin <ulmer.katrin4@gmail.com>
2026-03-22 16:55:45 +0100

    added changes to js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 const apiKey = "234180b1fac3cd52a404393etbea103o";
-const apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}';
-axios.get(apiUrl)
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
@@ -10,49 +8,49 @@ var cityElement = document.querySelector('.city');
 var tempElement = document.querySelector('.temp');
 var humidityElement = document.querySelector('.humidity');
 var windElement = document.querySelector('.wind');
-var weatherDetails = document.querySelector('.weather-details'); // This is the wrapper for all weather details
+var weatherDetails = document.querySelector('.weather-details');
 
-// Ensure the weather details are hidden initially
 weatherDetails.style.display = 'none';
 
 async function checkWeather(city) {
     if (!city) {
-        // If the search box is empty, hide all weather details
         weatherDetails.style.display = 'none';
         return;
     }
+
     
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const response = await fetch(
+        `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+    );
     var data = await response.json();
 
     console.log(data);
 
-    // If the city is found, show the weather details
-    if (data.cod === 200) {
-        weatherDetails.style.display = 'block'; // Show weather details if data is valid
+    
+    if (data.city) {
+        weatherDetails.style.display = 'block';
 
-        // Display the weather information
-        cityElement.innerHTML = data.name;
-        tempElement.innerHTML = Math.round(data.main.temp) + '°C';
-        humidityElement.innerHTML = data.main.humidity + '%';
+       
+        cityElement.innerHTML = data.city;
+        tempElement.innerHTML = Math.round(data.temperature.current) + '°C';
+        humidityElement.innerHTML = data.temperature.humidity + '%';
         windElement.innerHTML = data.wind.speed + ' km/h';
 
-        // Set the appropriate weather icon based on weather conditions
-        if (data.weather[0].main === 'Clouds') {
-            weatherIcon.src = 'assets/img/cloudy-forecast-svgrepo-com.svg';
-        } else if (data.weather[0].main === 'Clear') {
-            weatherIcon.src = 'assets/img/egg-sunny-side-up-svgrepo-com.svg';
-        } else if (data.weather[0].main === 'Rain') {
-            weatherIcon.src = 'assets/img/rain-svgrepo-com (2).svg';
-        } else if (data.weather[0].main === 'Drizzle') {
-            weatherIcon.src = 'assets/img/cloud-drizzle-svgrepo-com.svg';
-        } else if (data.weather[0].main === 'Mist') {
-            weatherIcon.src = 'assets/img/mist-svgrepo-com.svg';
-        } else if (data.weather[0].main === 'Snow') {
-            weatherIcon.src = 'assets/img/snowing-forecast-svgrepo-com.svg';
+        
+        if (data.condition.description.includes('cloud')) {
+            weatherIcon.src = 'src/img/cloudy-icon.svg';
+        } else if (data.condition.description.includes('clear')) {
+            weatherIcon.src = 'src/img/egg-sunny-side-up-icon.svg';
+        } else if (data.condition.description.includes('rain')) {
+            weatherIcon.src = 'src/img/rain-icon.svg';
+        } else if (data.condition.description.includes('drizzle')) {
+            weatherIcon.src = 'src/img/cloud-drizzle-icon.svg';
+        } else if (data.condition.description.includes('mist') || data.condition.description.includes('fog')) {
+            weatherIcon.src = 'src/img/mist-icon.svg';
+        } else if (data.condition.description.includes('snow')) {
+            weatherIcon.src = 'src/img/snowing-icon.svg';
         }
     } else {
-        // Handle case if the city is not found
         weatherDetails.style.display = 'none';
         alert("City not found!");
     }
@@ -62,7 +60,6 @@ searchBtn.addEventListener('click', () => {
     checkWeather(searchBox.value);
 });
 
-// Optionally, you can add a listener to handle the Enter key
 searchBox.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         checkWeather(searchBox.value);
